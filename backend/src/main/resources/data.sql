14b488ccadfa173c63d17840e31352be1f93a3e4
Diego Araujo <72672788+Dieggo775@users.noreply.github.com>
2026-04-11 16:19:46 -0300

    Ajustes Servidor

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,8 @@
--- Dados iniciais para o banco de dados DoeJa
--- Este arquivo será executado automaticamente pelo Spring Boot na inicialização
-
--- Exemplo de inserção de dados
--- INSERT INTO tabela (coluna1, coluna2) VALUES ('valor1', 'valor2');
+INSERT INTO centros (nome, endereco, latitude, longitude, telefone, horario) VALUES
+('Cruz Vermelha Taboão', 'Av. Piracema, 100 - Taboão da Serra - SP', -23.6263, -46.7680, '(11) 99999-0001', 'Seg-Sex 8h-18h'),
+('Hemocentro Municipal', 'Rua do Carmo, 250 - Taboão da Serra - SP', -23.6301, -46.7705, '(11) 99999-0002', 'Seg-Sáb 7h-17h'),
+('UBS Central Taboão', 'Praça Maria Thereza, 50 - Taboão da Serra', -23.6289, -46.7654, '(11) 99999-0003', 'Seg-Sex 7h-19h'),
+('Cruz Vermelha Itapecerica', 'R. José Manoel Gonçalves, 800 - Itapecerica da Serra', -23.7152, -46.8490, '(11) 99999-0004', 'Seg-Sex 9h-17h'),
+('Banco de Sangue Embu', 'Av. Miguel Marcello da Costa, 1200 - Embu das Artes', -23.6490, -46.8601, '(11) 99999-0005', 'Ter-Sáb 8h-16h'),
+('UBS Jardim Maria Rosa', 'R. Maria de Lourdes, 300 - Taboão da Serra', -23.6205, -46.7721, '(11) 99999-0006', 'Seg-Sex 7h-17h'),
+('Posto de Coleta São Paulo', 'R. Prof. Artur de Souza, 450 - Taboão da Serra', -23.6248, -46.7667, '(11) 99999-0007', 'Seg-Sex 8h-18h');
